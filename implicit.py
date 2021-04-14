f0a5c6843ea0fd71076048a831233d5b5aba84d4
alexeev509 <32396885+alexeev509@users.noreply.github.com>
2021-04-14 17:25:46 +0300

    Update implicit.py

--- implicit.py
+++ implicit.py
@@ -4,6 +4,7 @@ import implicit
 from scipy import sparse as sp
 
 
+
 example = pd.DataFrame(np.arange(12).reshape(3,4),columns=['a','b','c','d'])
 example.at[1, 'b'] = 0
 example.at[0, 'c'] = 0
@@ -15,13 +16,17 @@ dense_matrix = example.to_numpy()
 
 print(dense_matrix)
 
+dense_matrix=dense_matrix*0.1
+print("we multiply matrix on 0.1")
+print(dense_matrix)
+
 result_sparce_matrix=sp.coo_matrix(dense_matrix).tocsr()
 print(result_sparce_matrix)
 
-model = implicit.als.AlternatingLeastSquares(factors=16, regularization =0.0, iterations = 8)
+model = implicit.als.AlternatingLeastSquares(factors=16, regularization =0.01, iterations = 8)
 
 model.fit(result_sparce_matrix.T)
 
-query_sparce_matrix=sp.coo_matrix(np.array([[0,1,0,3]])).tocsr()
+query_sparce_matrix=sp.coo_matrix(np.array([[4,0,6,0]])).tocsr()
 
 print(model.recommend(0,query_sparce_matrix,N=4))
